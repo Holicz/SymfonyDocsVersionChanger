7b3b6ce4fd8246d8f0a45f5416e0e562b3deca21
Lukáš Holeczy <holi.4fan@gmail.com>
2016-08-06 16:16:12 +0200

    Refresh all tabs

--- popup.js
+++ popup.js
@@ -23,11 +23,42 @@ window.onload = function(){
 	 *
 	 */
 	function saveVersion() {
-		localStorage['version'] = document.getElementById('version').value;
+		localStorage['version'] = document.getElementById('version').value; // Save version
 
-	 	chrome.tabs.executeScript({
-	    	file: 'content_script.js'
-	 	}); 
+	 	if (localStorage['activity'] == 1) {
+		 	refreshTabs();
+		}
+	}
+
+	/**
+	 * Checks all tabs and redirect them to correct versions
+	 *
+	 */
+	function refreshTabs() {
+		chrome.windows.getAll({populate:true},function(windows){ // Affect all tabs with this settings
+			windows.forEach(function(window){
+			    window.tabs.forEach(function(tab){
+		    		var parser = document.createElement('a'); // Create an 'a' element for easiest parsing of URL
+			        parser.href = tab.url;
+
+			        var hostname = parser.hostname // Get website
+			        var pathname = parser.pathname; // Parse current url to get paths
+
+			        if (hostname == 'symfony.com') {
+			        	var paths = pathname.split("/");
+			        	var currentVersion = paths[2];
+			        	var wanterVersion = localStorage['version'];
+
+			        	if (currentVersion != wanterVersion && hostname == 'symfony.com' && paths[1] == 'doc') { // And finally if we are not in the right docs version...
+			          		paths[2] = wanterVersion;
+			          		pathname = paths.join('/'); // ...create new URL...
+
+			          		chrome.tabs.update(tab.id, {url: "https://symfony.com" + pathname}); // ...and let's redirect
+			        	}
+				     }
+			    });
+			  });
+		});
 	}
 
 	/**
@@ -48,9 +79,9 @@ window.onload = function(){
 			changeButton('disabled', 'Disable');
 		}
 
-		chrome.tabs.executeScript({
-	    	file: 'content_script.js'
-	  	}); 
+		if (localStorage['activity'] == 1) {
+		 	refreshTabs();
+		}
 	}
 
 	function changeButton(classname, label) {
