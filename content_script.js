3d0b36b0d3794b549e212167986749ebe5185e8a
Lukas Holeczy <info@lukasholeczy.eu>
2018-03-23 18:20:00 +0100

    Reformat JS code

--- content_script.js
+++ content_script.js
@@ -1,34 +1,34 @@
 chrome.runtime.sendMessage(
-  {method: "getVersion"}, // Get wanted version 
-  function(response) {
-    var wanterVersion = response.data;
+    {method: "getVersion"}, // Get wanted version
+    function (response) {
+        var wanterVersion = response.data;
 
-    chrome.runtime.sendMessage(
-      {method: "getActivity"}, // Check if the extension is enabled
-      function(activity) {
-        if (activity.data == 1) {
-          var url = document.location.href; // Get current URL
+        chrome.runtime.sendMessage(
+            {method: "getActivity"}, // Check if the extension is enabled
+            function (activity) {
+                if (activity.data === 1) {
+                    var url = document.location.href; // Get current URL
 
-          if (!wanterVersion) { // In case this is the first time the extension loaded (no wantedVersion set)
-          	wanterVersion = 'current';
-        	}
+                    if (!wanterVersion) { // In case this is the first time the extension loaded (no wantedVersion set)
+                        wanterVersion = 'current';
+                    }
 
-        	var parser = document.createElement('a'); // Create an 'a' element for easiest parsing of URL
-          parser.href = url;
+                    var parser = document.createElement('a'); // Create an 'a' element for easiest parsing of URL
+                    parser.href = url;
 
-          var hostname = parser.hostname // Get website
-          var pathname = parser.pathname; // Parse current url to get paths
-          var paths = pathname.split("/");
-          var currentVersion = paths[2];
+                    var hostname = parser.hostname; // Get website
+                    var pathname = parser.pathname; // Parse current url to get paths
+                    var paths = pathname.split("/");
+                    var currentVersion = paths[2];
 
-          if (currentVersion != wanterVersion && hostname == 'symfony.com' && paths[1] == 'doc') { // And finally if we are not in the right docs version...
-          	paths[2] = wanterVersion;
-          	pathname = paths.join('/'); // ...create new URL...
+                    if (currentVersion !== wanterVersion && hostname === 'symfony.com' && paths[1] === 'doc') { // And finally if we are not in the right docs version...
+                        paths[2] = wanterVersion;
+                        pathname = paths.join('/'); // ...create new URL...
 
-          	chrome.runtime.sendMessage({redirect: "https://symfony.com" + pathname}); // ...and let's redirect
-          }
-        }
-      }
-    );
-  }
+                        chrome.runtime.sendMessage({redirect: "https://symfony.com" + pathname}); // ...and let's redirect
+                    }
+                }
+            }
+        );
+    }
 );
